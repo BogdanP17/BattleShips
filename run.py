d5f47dc38da3f4c0a5f02034280348ed30777192
Bogdan Petrutescu <petrutescubogdan@yahoo.com>
2024-02-25 01:35:30 +0000

    Check when the game is finish

--- run.py
+++ run.py
@@ -1,110 +1,67 @@
 import random
 
 
-board = [['0', '0', '0'],
-         ['0', '0', '0'],
-         ['0', '0', '0']]
-
-
+HIDDEN_BOARD = [['0' for x in range(3)] for x in range(3)]
+GUESS_BOARD = [['0' for x in range(3)] for x in range(3)]
 
 def print_board(board):
-    """
-    Print the board game 
-    """
     for row in board:
         print(" ".join(row))
 
-
-def palce_ship(board):
-    """
-    Create the ships on the board
-    """
+def place_ship(HIDDEN_BOARD):
     for i in range(3):
-        row = random.randint(0, len(board) - 1)
-        col = random.randint(0, len(board[0]) - 1)
-        while board[row][col] != '0':
-            row = random.randint(0, len(board) - 1)
-            col = random.randint(0, len(board[0]) - 1)
-        board[row][col] = 'S'
-
+        row = random.randint(0, len(HIDDEN_BOARD) - 1)
+        col = random.randint(0, len(HIDDEN_BOARD[0]) - 1)
+        while HIDDEN_BOARD[row][col] != '0':
+            row = random.randint(0, len(HIDDEN_BOARD) - 1)
+            col = random.randint(0, len(HIDDEN_BOARD[0]) - 1)
+        HIDDEN_BOARD[row][col] = 'S'
 
 def get_guess():
-    """
-    Create the guesses function
-    """
     while True:
         guess = input("Enter your (row column): ").split()
         if len(guess) != 2:
             print("Invalid input. Please enter a row and a column separated by space")
             continue
         try:
-            row = int(guess[0])
-            col = int(guess[1])
+            row = int(guess[0]) 
+            col = int(guess[1]) 
+            if row < 0 or row > 3 or col < 0 or col > 3:
+                raise ValueError
         except ValueError:
-            print("Invalid input. Please enter numbers as integers")
+            print(f"Invalid input. Please enter numbers as integers")
             continue
-        if row < 1 or row > 3 or col < 1 or col > 3:
-            print("Invalid input. Row and column must be between 1 and 3")
-            continue
-        return row - 1, col - 1
-
+        return row -1, col -1
+    
+    
+def check_win(GUESS_BOARD):
+    all(all(cell != 'S' for cell in row) for row in GUESS_BOARD)
 
 def battleship():
-    """
-    Create the game function
-    """
     print("Start play BattleShip!")
-    print_board(board)
-    
     print("Ships placed!")
-    print_board(board)
-
-    turns = 0
+    place_ship(GUESS_BOARD)
+    
+    turns = 4
     while True:
-        print(f"Turn {turns + 1}:")
-        print_board(board) 
-        print()
-        print("Player 1's turn:")
+        print_board(HIDDEN_BOARD)
+        print("Player's 1 turn:")
         guess_row, guess_col = get_guess()
-        
-        if board[guess_row][guess_col] == 'S':
+        if GUESS_BOARD[guess_row][guess_col] == 'S':
             print("Congratulations! You hit a ship!")
-            board[guess_row][guess_col] = 'X'
+            HIDDEN_BOARD[guess_row][guess_col] = 'X'
         else:
             print("Sorry, you missed!")
 
-        
-        print_board(board)
-        print()
-        
-        if all(all(cell != 'S' for cell in row) for row in board):
-            print("Player 1 wins!")
-            break
-
-        turns += 1
-
-        print(f"Turn {turns + 1}:")
-        print_board(board)
-        print()
-        print("Player 2's turn:")
-        guess_row, guess_col = get_guess()
-        
-        if board[guess_row][guess_col] == 'S':
-            print("Congratulations! You hit a ship!")
-            board[guess_row][guess_col] = 'X'
-        else:
-            print("Sorry, you missed!")
-        
-        print_board(board)
-        print()
-        
-        if all(all(cell != 'S' for cell in row) for row in board):
-            print("Player 2 wins!")
+        if check_win(GUESS_BOARD):
+            print('Congratulations! You sank all the ships!')
             break
         
-        turns += 1
-
+        turns -= 1
+        if turns == 0:
+            print('Game Over! Sorry you dont have any more guesses.')
+            break
+            
+    
 
 battleship()
-
-
